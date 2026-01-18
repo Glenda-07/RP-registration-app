b7122dd05d32180f663ad4ac1450af1763c4fe81
Glendaa <24021152@myrp.edu.sg>
2026-01-18 15:13:32 +0800

    Add course input to registration form
    Added a course input field to the registration form and updated the handleSubmit function to include the course in the navigation state.

--- src/components/Register.js
+++ src/components/Register.js
@@ -5,20 +5,28 @@ export default function Register() {
   const navigate = useNavigate();
   const nameRef = useRef(null);
   const emailRef = useRef(null);
+  const courseRef = useRef(null);
+
 
   function handleSubmit(event) {
     event.preventDefault();
     navigate("/confirmed", {state: { name:nameRef.current.value, email: emailRef.
-    current.value }});
+    current.value, course:courseRef.current.value }});
   }
 
   return (
     <div className="container">
-      <h1>Register for Red30 Tech</h1>
-      <p>
-        Make sure to grab your spot for this year's conference! We love
-        technology and consistently work towards being the premier provider of
-        technology solutions and events that connect the world.
+      <h1 className="middle">Register your interest now!</h1>
+      <p className="middle">
+        Register your interest in a diploma course by filling in the form below.
+        <br></br>
+        <strong className="middle">
+          <h3>
+            Course available now: 
+          </h3>
+        </strong>
+        <p className="middle">Information Technology</p>
+        <p className="middle">Financial Technology</p>
       </p>
       <form onSubmit={handleSubmit}>
         <label>
@@ -28,6 +36,10 @@ export default function Register() {
         <label>
           Email:
           <input type="text" name="email" ref={emailRef} />
+        </label>
+        <label>
+          Course:
+          <input type="text" name="course" ref={courseRef} />
         </label>
         <input type="submit" value="Submit" />
       </form>
