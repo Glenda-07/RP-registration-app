df9d6c1b030f8e7bf98a4ac57644716fa820869c
Glendaa <24021152@myrp.edu.sg>
2026-01-18 15:12:25 +0800

    Refactor Modules component to display sessions

--- src/components/Modules.js
+++ src/components/Modules.js
@@ -2,36 +2,25 @@ import { useParams, NavLink, Outlet } from "react-router-dom";
 import { getModules } from "../api";
 
 export default function Modules() {
-  const { schoolId, moduleId } = useParams();
-  const module = getModules( moduleId);
-  
-  if (!module) {
-    return <p>Select a Diploma to see modules.</p>;
-  }
+  const { moduleId } = useParams(); // this is the diplomaId
+const sessions = getModules(moduleId);
 
-  return (
-    <>
-      <h2>{module.name} Sessions</h2>
+if (!sessions.length) return <p>Select a diploma to see modules.</p>;
 
-      <ul className="session-list">
-        {module.sessions.map(session => (
-          <li className="session" key={session.id}>
-            <NavLink
-              to={session.id}
-              className={({ isActive }) =>
-                isActive ? "session-active" : null
-              }
-            >
-              <p className="session-name">{session.name}</p>
-              <p>
-                {session.speaker.name} | {session.speaker.org}
-              </p>
-            </NavLink>
-          </li>
-        ))}
-      </ul>
+return (
+  <>
+    <h2>Modules</h2>
+    <ul className="session">
+      {sessions.map(session => (
+        <li key={session.id}>
+          <NavLink to={session.id}>
+            <p>{session.name}</p>
+          </NavLink>
+        </li>
+      ))}
+    </ul>
+    <Outlet />
+  </>
+);
 
-      <Outlet />
-    </>
-  );
 }
