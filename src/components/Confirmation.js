f537f18dc492898df2c92abf84dc025e3c9cf623
Glendaa <24021152@myrp.edu.sg>
2026-01-18 15:10:42 +0800

    Update confirmation message for registration

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -5,11 +5,11 @@ export default function Confirmation() {
 
   return (
     <div className="container">
-      <h1>Thank You!</h1>
+      <h1>Thank You For Registering!</h1>
       
       {state && (
       <>
-        <p>{state.name}, you're now registered for Red30 Tech.</p>
+        <p>{state.name}, you have been registered for {state.course}.</p>
       <p>
         We've sent more details to <strong>{state.email}</strong>.
       </p>
